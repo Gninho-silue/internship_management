671af8d76e1cdf9d9454c6b082aa956d3d7e8d46
SILUE GNINNINMAGUIGNON <gninhosilue@gmail.com>
2025-08-26 19:36:25 +0100

    Setup Intership Management Module

---
+++ custom-addons/internship_management/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# Import des sous-modules
+from . import models
+# from . import controllers  # A décommenter quand vous créerez des controllers
+# from . import wizard       # A décommenter si vous créez des wizards
+# from . import reports      # A décommenter pour les rapports personnalisés
