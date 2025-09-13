4b7c838f530e3c45032ad3f79c372f06f4757d71
SILUE GNINNINMAGUIGNON <gninhosilue@gmail.com>
2025-09-13 21:36:25 +0100

    Remove obsolete messaging and notification models
    - Deleted internship_message.py (replaced by unified communication system)
    - Deleted internship_notification.py (replaced by unified communication system)
    - Deleted internship_message_views.xml (replaced by new views)
    - Deleted internship_notification_views.xml (replaced by new views)
    - Updated models/__init__.py to remove obsolete imports
    
    This cleanup prepares for the new unified communication system
    that respects the requirements document.

--- custom-addons/internship_management/models/__init__.py
+++ custom-addons/internship_management/models/__init__.py
@@ -5,7 +5,5 @@ from . import internship_student
 from . import internship_stage
 from . import internship_document
 from . import internship_supervisor
-from . import internship_message
-from . import internship_notification
 from . import internship_meeting
 from . import internship_user_profile
